f69f339d8aad2ac52d7dc9a1a40d5f4acac533d4
Kartagis <tolga@ozses.net>
2021-01-13 20:46:30 +0300

    Order paid subscriber

--- src/Form/AydinlikMessagesForm.php
+++ src/Form/AydinlikMessagesForm.php
@@ -32,33 +32,38 @@ class AydinlikMessagesForm extends ConfigFormBase {
         $form['girisyapmesaji'] = [
             '#type' => 'text_format',
             '#title' => 'Misafir kullanıcılara gösterilecek mesaj',
-            '#default_value' => $config->get('girisyapmesaji'),
+            '#default_value' => $config->get('girisyapmesaji.value'),
             '#format' => 'full_html',
         ];
         $form['satinalmesaji'] = [
             '#type' => 'text_format',
             '#title' => 'Kayıtlı kullanıcı olup da e-gazete aboneliği olmayanlara gösterilecek satın alın mesajı',
-            '#default_value' => $config->get('satinalmesaji'),
+            '#default_value' => $config->get('satinalmesaji.value'),
         ];
         $form['icerikaboneligiaraligimesaji'] = [
             '#type' => 'text_format',
             '#title' => 'İçerik aboneliği e-gazetenin çıkış tarihi aralığında olmayanlara gösterilecek mesaj',
-            '#default_value' => $config->get('icerikaboneligiaraligimesaji'),
+            '#default_value' => $config->get('icerikaboneligiaraligimesaji.value'),
         ];
         $form['earsivabonesidegilmesaji'] = [
             '#type' => 'text_format',
             '#title' => 'E-arşiv aboneliği olmayanlara gösterilecek mesaj',
-            '#default_value' => $config->get('earsivabonesidegilmesaji'),
+            '#default_value' => $config->get('earsivabonesidegilmesaji.value'),
+        ];
+        $form['abonelikaktifdegilmesaji'] = [
+            '#type' => 'text_format',
+            '#title' => 'Aboneliği aktif olmayanlara gösterilecek mesaj',
+            '#default_value' => $config->get('abonelikaktifdegilmesaji.value'),
         ];
         $form['egazeteaboneliksuresibittimesaji'] = [
             '#type' => 'text_format',
             '#title' => 'E-gazete abonelik süresi bitenlere gösterilecek mesaj',
-            '#default_value' => $config->get('egazeteaboneliksuresibittimesaji'),
+            '#default_value' => $config->get('egazeteaboneliksuresibittimesaji.value'),
         ];
         $form['egazeteaboneliksuresibitecekmesaji'] = [
             '#type' => 'text_format',
             '#title' => 'E-gazete abonelik süresi bitmek üzere olanlara gösterilecek mesaj',
-            '#default_value' => $config->get('egazeteaboneliksuresibitecekmesaji'),
+            '#default_value' => $config->get('egazeteaboneliksuresibitecekmesaji.value'),
         ];
         $form['kaydet'] = [
             '#type' => 'submit',
@@ -78,14 +83,15 @@ class AydinlikMessagesForm extends ConfigFormBase {
      */
     public function submitForm(array &$form, FormStateInterface $form_state) {
         $config = $this->configFactory()->getEditable('aydinlik.settings');
-        $config->set('girisyapmesaji', $form_state->getValue('girisyapmesaji'));
-        $config->set('satinalmesaji', $form_state->getValue('satinalmesaji'));
-        $config->set('icerikaboneligiaraligimesaji', $form_state->getValue('icerikaboneligiaraligimesaji'));
-        $config->set('earsivabonesidegilmesaji', $form_state->getValue('earsivabonesidegilmesaji'));
-        $config->set('egazeteaboneliksuresibittimesaji', $form_state->getValue('egazeteaboneliksuresibittimesaji'));
-        $config->set('egazeteaboneliksuresibitecekmesaji', $form_state->getValue('egazeteaboneliksuresibitecekmesaji'));
-        $config->set('aboneliksuresimesaji', $form_state->getValue('aboneliksuresimesaji'));
+        $config->set('girisyapmesaji', $form_state->getValue('girisyapmesaji'))->value;
+        $config->set('satinalmesaji', $form_state->getValue('satinalmesaji'))->value;
+        $config->set('icerikaboneligiaraligimesaji', $form_state->getValue('icerikaboneligiaraligimesaji'))->value;
+        $config->set('earsivabonesidegilmesaji', $form_state->getValue('earsivabonesidegilmesaji'))->value;
+        $config->set('abonelikaktifdegilmesaji', $form_state->getValue('abonelikaktifdegilmesaji'))->value;
+        $config->set('egazeteaboneliksuresibittimesaji', $form_state->getValue('egazeteaboneliksuresibittimesaji'))->value;
+        $config->set('egazeteaboneliksuresibitecekmesaji', $form_state->getValue('egazeteaboneliksuresibitecekmesaji'))->value;
         $config->save();
+        $test = '';
         return parent::submitForm($form, $form_state);
     }
 }
